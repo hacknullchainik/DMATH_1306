2e4c0b566bbefe0a9948ed69a8921c166b1664a7
hacknullchainik <zag2pay@gmail.com>
2022-03-22 20:09:19 +0300

    Welcome t t cmzone!

---
+++ main.py
@@ -0,0 +1,58 @@
+# Комментарии к оформлению:
+# Пока что пишем модулями(т.е. каждый назывет модуль типа ИМЯФ.py и подключает сюда (название на инглише, прим SEVAB.py)
+# В модуле ваши функции, которые вызываются через точку типа(по сути просто пишите ваши функции в :
+# import SEVAB
+# SEVAB.funct_name()
+
+# Адекватно называем переменные(кроме счётчиков циклов)
+# Юзаем как можно больше комментариев (чтобы было понятно, че вы там написали)
+# Между логическими частями ставьте пробелы, чтобы красиво (особенно перед комментариями)
+# Ну и оставшиеся правила:
+# Читы - бан
+# Стельба по своим - бан
+# Оскорбление администрации - расстрел, а потом бан.
+
+class NNubmer:
+    # Классическая инициализация
+    def __init__(self,number:list):
+        # value - значение, то бишь массив цифр
+        self.value = number
+        # Последний разряд, то бишь если число длины 5, то это 4(т.к. нумерация с нуля)
+        self.rank = len(number)-1
+
+
+class Integer:
+    # Классическая инициализация
+    # sign выбрал за бул, чтобы меньше шансов было упороться
+    # Знак рассматриваем как True = '-', False = '+' (1 - есть, 0 - нет)
+    # П.с. к инту кастуйте и не ебите себе мозги int(True) == 1
+    def __init__(self, number: list, sign:bool):
+        # value - значение, то бишь массив цифр
+        self.value = number
+        # Последний разряд, то бишь если число длины 5, то это 4(т.к. нумерация с нуля)
+        self.rank = len(number) - 1
+        # Знак
+        self.sign = sign
+
+
+class RNumber:
+    # числитель, знаменатель - всё просто
+    def __init__(self, numerator:int, denominator:int):
+        self.num = numerator
+        self.den = denominator
+        if self.num < 0 and self.den < 0:
+            self.num = abs(self.num)
+            self.den = abs(self.den)
+        elif self.num < 0 or self.den < 0:
+            if self.den < 0:
+                self.num = -self.num
+                self.den = -self.den
+
+
+class Polynomial:
+    # ЭТТЕНШН крч
+    # Список коэффициентов пусть будет списком из объектов класса Rnumber
+    def __init__(self, coefficients:list, exponent:int):
+        self.coefs = coefficients
+        # Макс степень
+        self.exp = exponent
