cbbe47df2b0ae231f8d4277dccf9dedf07541ef5
Sasha Pulinets <sasha.hellsys@gmail.com>
2022-03-23 15:03:13 +0300

    test

--- SASHAP.py
+++ SASHAP.py
@@ -1,3 +1,16 @@
+import Dtypes
 from Dtypes import *
 
-ptint("aboba")
+def zero_check(num):
+    if num != 0:
+        return True
+    else:
+        return False
+
+
+num1 = Dtypes.NNubmer(int(input))
+
+print(zero_check(num1))
+
+# def ADD_NN_N(number1, number2):
+#
