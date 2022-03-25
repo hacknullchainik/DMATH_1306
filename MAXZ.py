51c5f2bd2fa97a27d1e317d55215ec5f62d86349
MaximZinovyev <zinovyevmaxim@gmail.com>
2022-03-25 21:20:16 +0300

    TRANS_Q_Z added. Добавлен комментарий.

--- MAXZ.py
+++ MAXZ.py
@@ -1,5 +1,7 @@
 from Dtypes import Integer, NNumber, RNumber, Polynomial
 
+#Проверка на целое, если рациональное
+#число является целым,то «да», иначе «нет»
 def INT_Q_B(num: RNumber):
     # проверяем равен ли знаменатель 1
     if num.get_den() == 1:
