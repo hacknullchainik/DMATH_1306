be10415eacd50f3d67ff9a2205c76d92de0752f8
MaximZinovyev <zinovyevmaxim@gmail.com>
2022-03-26 19:53:53 +0300

    Special for kazah

--- MAXZ.py
+++ MAXZ.py
@@ -17,3 +17,16 @@ def LCM_NN_N(num1: NNumber, num2: NNumber):
     #найдем НОД двух чисел и НОК разделим на НОД
     #НОК(a,b)=a*b/НОД(a,b)
     return (mult // GCF_NN_N(num1, num2))
+
+
+#Деление дробей (делитель отличен от нуля)
+def DIV_QQ_Q(n1: RNumber, n2: RNumber):
+    num1 = TRANS_Q_Z(RNumber(n1.get_num(), 1))
+    num2 = TRANS_Q_Z(RNumber(n2.get_num(), 1))
+    den1 = TRANS_Q_Z(RNumber(n1.get_den(), 1))
+    den2 = TRANS_Q_Z(RNumber(n2.get_den(), 1))
+    result_num = MUL_ZZ_Z(num1, den2)
+    result_den = MUL_ZZ_Z(den1, num2)
+    return RNumber(result_num.get_num(), result_den.get_num())
+
+print(DIV_QQ_Q(RNumber(1,2), RNumber(2,3)))
