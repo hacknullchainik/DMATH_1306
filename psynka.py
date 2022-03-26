62dbe922489cb77d04a438881c4a7bb6949a1173
psynka <temich550@gmail.com>
2022-03-26 17:49:13 +0300

    aye

---
+++ psynka.py
@@ -0,0 +1,11 @@
+from Dtypes import Integer, NNumber
+#from ALEX import TRANS_N_Z
+
+def TRANS_Z_N(num: Integer):
+    return NNumber(num.get_num())
+
+#my_num = Integer([1,2,3], False)
+#print(TRANS_Z_N(my_num))
+
+#his_num = NNumber([1,2,3])
+#print(TRANS_N_Z())
