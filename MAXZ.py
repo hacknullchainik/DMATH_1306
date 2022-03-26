e9facd4ad3e1585e44f6fde8fb3dbec124fc6d1f
MaximZinovyev <zinovyevmaxim@gmail.com>
2022-03-26 18:30:49 +0300

    Добавлена функция LCM_NN_N.

--- MAXZ.py
+++ MAXZ.py
@@ -8,3 +8,11 @@ def INT_Q_B(num: RNumber):
         return True
     else:
         return False
+
+#НОК натуральных чисел
+def LCM_NN_N(num1:NNumber, num2:NNumber):
+    #найдем произведение двух чисел:
+    mult=MUL_NN_N(num1, num2)
+    #найдем НОД двух чисел и НОК разделим на НОД
+    #НОК(a,b)=a*b/НОД(a,b)
+    result (mult // GCF_NN_N(num1, num2))
