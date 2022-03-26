33b7593b2565470504d19c2199ef6231ab199532
psynka <temich550@gmail.com>
2022-03-26 20:44:40 +0300

    DIV_QQ_Q

--- psynka.py
+++ psynka.py
@@ -1,6 +1,15 @@
-from Dtypes import Integer, NNumber
+from Dtypes import Integer, NNumber, Polynomial, RNumber
 
 def TRANS_Z_N(num: Integer):
     num = num.get_num()
     num.reverse()
     return NNumber(num)
+
+def LED_P_Q(mchlen: Polynomial):
+    return mchlen.get_coefs()[len(mchlen.get_coefs())-1]
+
+#f = RNumber(1,5)
+#s = RNumber(2)
+#t = RNumber(3)
+#mchlen = Polynomial([f,s,t])
+#print(LED_P_Q(mchlen))
