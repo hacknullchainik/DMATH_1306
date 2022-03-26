71fff1d437492a48b1e90d4348a08b8453a5eab9
MaximZinovyev <zinovyevmaxim@gmail.com>
2022-03-26 21:05:49 +0300

    Добавлена функция DIV_QQ_Q.

--- MAXZ.py
+++ MAXZ.py
@@ -21,13 +21,32 @@ def LCM_NN_N(num1: NNumber, num2: NNumber):
 
 #Деление дробей (делитель отличен от нуля)
 def DIV_QQ_Q(n1: RNumber, n2: RNumber):
-    num1 = TRANS_Q_Z(RNumber(n1.get_num(), 1))
-    num2 = TRANS_Q_Z(RNumber(n2.get_num(), 1))
-    den1 = TRANS_Q_Z(RNumber(n1.get_den(), 1))
-    den2 = TRANS_Q_Z(RNumber(n2.get_den(), 1))
+
+    #берутся отдельно числители(num1, num2, с учетом их знака) и знаменатели(den1, den2)
+    if n1.get_num()>0:
+        num1 = Integer(list(str(n1.get_num())), False)
+    else:
+        num1 = Integer(list(str(n1.get_num())[1:]), True)
+
+    if n2.get_num()>0:
+        num2 = Integer(list(str(n2.get_num())), False)
+    else:
+        num2 = Integer(list(str(n2.get_num())[1:]), True)
+
+    den1 = Integer(list(str(n1.get_den())), False)
+    den2 = Integer(list(str(n2.get_den())), False)
+
+    #первый числитель умножается на второй знаменатель,
+    #а первый знаменатель умножается на второй числитель
+    # результирующий числитель и знаменатель записываются в result_num и result_den, соответственно.
     result_num = MUL_ZZ_Z(num1, den2)
     result_den = MUL_ZZ_Z(den1, num2)
 
-    return RNumber(int(result_num.get_num()[0]), int(result_den.get_num()[0]))
-
-print(DIV_QQ_Q(RNumber(1,2), RNumber(2,3)))
+    #если знаки результирующего числителя и знаменателя одинаковы - результат положителен,
+    #иначе - отрицателен
+    if result_num.get_sign()==result_den.get_sign():
+        return RNumber(int(''.join(map(str, result_num.get_num()[::-1]))),
+                       int(''.join(map(str, result_den.get_num()[::-1]))))
+    else:
+        return RNumber(-int(''.join(map(str, result_num.get_num()[::-1]))),
+                       int(''.join(map(str, result_den.get_num()[::-1]))))
