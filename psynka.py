c019d3f5d59be80d9536a7a15a83ec4bd4f5bfd9
psynka <temich550@gmail.com>
2022-03-28 23:43:17 +0300

    MUL_QQ_Q

--- psynka.py
+++ psynka.py
@@ -12,18 +12,18 @@ def LED_P_Q(mchlen: Polynomial):
 
 def MUL_QQ_Q(n1: RNumber, n2: RNumber):
     # берутся отдельно числители(num1, num2, с учетом их знака) и знаменатели(den1, den2)
-    if n1.get_num() > 0:
+    if n1.get_sign() == False:
         num1 = Integer([int(i) for i in str(n1.get_num()) if '0' <= i <= '9'], False)
     else:
         num1 = Integer([int(i) for i in str(n1.get_num()) if '0' <= i <= '9'], True)
 
-    if n2.get_num() > 0:
+    if n2.get_sign() == False:
         num2 = Integer([int(i) for i in str(n2.get_num()) if '0' <= i <= '9'], False)
     else:
         num2 = Integer([int(i) for i in str(n2.get_num()) if '0' <= i <= '9'], True)
 
-    den1 = Integer([int(i) for i in str(n1.get_den()) if '0' <= i <= '9'], False)
-    den2 = Integer([int(i) for i in str(n2.get_den()) if '0' <= i <= '9'], False)
+    den1 = NNumber([int(i) for i in str(n1.get_den()) if '0' <= i <= '9'])
+    den2 = NNumber([int(i) for i in str(n2.get_den()) if '0' <= i <= '9'])
 
     # первый числитель умножается на второй,
     # а первый знаменатель умножается на второй
@@ -40,9 +40,9 @@ def MUL_QQ_Q(n1: RNumber, n2: RNumber):
         return RNumber(-int(''.join(map(str, result_num.get_num()[::-1]))),
                        int(''.join(map(str, result_den.get_num()[::-1]))))
 
-n1 = RNumber(10,2)
-n2 = RNumber(-2,3)
-print(MUL_QQ_Q(n1,n2))
+#n1 = RNumber('10','2')
+#n2 = RNumber('-2','3')
+#print(MUL_QQ_Q(n1,n2))
 
 
 #f = RNumber(1,5)
