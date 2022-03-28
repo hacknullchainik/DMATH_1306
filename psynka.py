0a3929e029281d4c8bc9d308c6d93f45fa2e8b43
psynka <temich550@gmail.com>
2022-03-29 00:06:28 +0300

    MUL_QQ_Q

--- psynka.py
+++ psynka.py
@@ -1,5 +1,5 @@
 from Dtypes import Integer, NNumber, Polynomial, RNumber
-from SASHAP import MUL_ZZ_Z
+from SASHAP import MUL_ZZ_Z, MUL_NN_N
 
 
 def TRANS_Z_N(num: Integer):
@@ -29,20 +29,23 @@ def MUL_QQ_Q(n1: RNumber, n2: RNumber):
     # а первый знаменатель умножается на второй
     # результирующий числитель и знаменатель записываются в result_num и result_den, соответственно.
     result_num = MUL_ZZ_Z(num1, num2)
-    result_den = MUL_ZZ_Z(den1, den2)
+    result_den = MUL_NN_N(den1, den2)
+
 
     # если знаки результирующего числителя и знаменателя одинаковы - результат положителен,
     # иначе - отрицателен
-    if result_num.get_sign() == result_den.get_sign():
-        return RNumber(int(''.join(map(str, result_num.get_num()[::-1]))),
-                       int(''.join(map(str, result_den.get_num()[::-1]))))
+    if result_num.get_sign() == False:
+        result_num = result_num.get_num()[::-1]
+        result_den = result_den.get_num()[::-1]
+        return RNumber(Integer(result_num ,False), NNumber(result_den))
     else:
-        return RNumber(-int(''.join(map(str, result_num.get_num()[::-1]))),
-                       int(''.join(map(str, result_den.get_num()[::-1]))))
+        result_num = result_num.get_num()[::-1]
+        result_den = result_den.get_num()[::-1]
+        return RNumber(Integer(result_num,True), NNumber(result_den))
 
-#n1 = RNumber('10','2')
-#n2 = RNumber('-2','3')
-#print(MUL_QQ_Q(n1,n2))
+n1 = RNumber('25','2')
+n2 = RNumber('-2','30')
+print(MUL_QQ_Q(n1,n2))
 
 
 #f = RNumber(1,5)
