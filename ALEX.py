d3092f22cde28c3bae126f70d548adf62f50ff5c
Alex Bulganin <90492542+doal116@users.noreply.github.com>
2022-03-30 14:39:25 +0300

    Update ALEX.py

--- ALEX.py
+++ ALEX.py
@@ -1,7 +1,9 @@
 # The present module works with Integer, RNumber, Polynomial, NNumber instances
 # The module providing those classes is 'Dtypes.py'
 from Dtypes import Integer, NNumber
-
+from NIKITAT import DIV_ZZ_Z
+from SASHAP import MUL_ZZ_Z
+# Nastia's file lacking SUB_ZZ_Z
 
 def MUL_ND_N(num: object, num_2: int):
     # local variables storing the value from arugments
@@ -51,3 +53,30 @@ def MUL_ND_N(num: object, num_2: int):
 # depending on the user preference.
 def TRANS_N_Z(num:NNumber):
     return Integer(num.get_num(), False)
+
+#------------function partially completed-----------#
+#------------ SUB_ZZ_Z not ready -------------------#
+#The function looks for the remainder of a division between two integers 
+def MOD_ZZ_Z(num:Integer, num_2:Integer):
+    #The function finds the remainder by using the euclidian theorem.
+    #'a' stores the value of the dividend.
+    #'b' stores the value of the divisor.
+    #'q' stores the value of the quotient.
+    #'r' stores the value of the remainder.
+    
+    a = Integer(num.get_num()[::-1],num.get_sign())
+    b = Integer(num_2.get_num()[::-1],num_2.get_sign())
+    
+    # DIV_ZZ_Z FROM NIKITAT.PY
+    # Finding the quotient from the division of an integer by an integer
+    q = DIV_ZZ_Z(a,b)
+
+    # MUL_ZZ_Z FROM SASHAP.PY
+    # Storing the value of the divisor multiplied by the quotient
+    a_1 = MUL_ZZ_Z(a,q)
+
+    # SUB_ZZ_Z() -> pending from Nastia <-
+    # Substructing the value of the divident multiplied by the quotient from the dividend
+    r = SUB_ZZ_Z(a,a_1)
+
+    return r
