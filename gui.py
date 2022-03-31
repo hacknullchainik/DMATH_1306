fd4aba7b1980a3e771f741b64e44dacd4cd54bcc
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-03-31 12:18:59 +0300

    add integer, rational functions, titles for polynomials

--- gui.py
+++ gui.py
@@ -412,7 +412,7 @@ def MUL_ZZ_Z():
         result.grid(column=1, row=1)
         result.insert(1.0, "({0}) * ({1}) = {2}".format(str(a_val), str(b_val),
                                                         str(Integers.MUL_ZZ_Z(a_val, b_val))))
-        result.configure(state='disabled')\
+        result.configure(state='disabled')
 
     try:
         frame_output.destroy()
@@ -457,8 +457,8 @@ def DIV_ZZ_Z():
         result = Text(frame_output)
         result.grid(column=1, row=1)
         result.insert(1.0, "({0}) div ({1}) = {2}".format(str(a_val), str(b_val),
-                                                        str(Integers.MUL_ZZ_Z(a_val, b_val))))
-        result.configure(state='disabled')\
+                                                        str(Integers.DIV_ZZ_Z(a_val, b_val))))
+        result.configure(state='disabled')
 
     try:
         frame_output.destroy()
@@ -479,7 +479,6 @@ def DIV_ZZ_Z():
     b = Entry(frame_input, width=16)
     b.grid(row=2, column=2, padx=(4, 0))
     but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
-
 
 # Остаток от деления двух чисел
 def MOD_ZZ_Z():
@@ -504,8 +503,8 @@ def MOD_ZZ_Z():
         result = Text(frame_output)
         result.grid(column=1, row=1)
         result.insert(1.0, "({0}) mod ({1}) = {2}".format(str(a_val), str(b_val),
-                                                        str(Integers.MUL_ZZ_Z(a_val, b_val))))
-        result.configure(state='disabled')\
+                                                        str(Integers.MOD_ZZ_Z(a_val, b_val))))
+        result.configure(state='disabled')
 
     try:
         frame_output.destroy()
@@ -528,12 +527,194 @@ def MOD_ZZ_Z():
     but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
 
 
+# RATIONALS NUMBERS
+# Сокращение дроби
+def RED_Q_Q():
+    global frame_output, frame_input
+
+    def RESULT():
+        global frame_output
+        a_val = RNumber(str(a.get()))
+        frame_input.destroy()
+        frame_output = Frame(window)
+        frame_output.grid()
+        window.title("Результат сокращения дроби")
+        result = Text(frame_output)
+        result.grid(column=1, row=1)
+        result.insert(1.0, "{0} = {1}".format(a_val, Rationals.RED_Q_Q(a_val)))
+        result.configure(state='disabled')
+
+    try:
+        frame_output.destroy()
+    except:
+        print("none")
+    try:
+        frame_input.destroy()
+    except:
+        print("none")
+    frame.destroy()
+    frame_input = Frame(window)
+    frame_input.grid()
+    window.title("Сокращение дроби")
+    a_lab = Label(frame_input, text="Введите дробь:").grid(row=1, column=1)
+    a = Entry(frame_input, width=16)
+    a.grid(row=1, column=2, padx=(4, 0))
+    but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
+
+# Сложение дробей
+def ADD_QQ_Q():
+    global frame_output, frame_input
+
+    def RESULT():
+        global frame_output
+        a_val = RNumber(str(a.get()))
+        b_val = RNumber(str(b.get()))
+        frame_input.destroy()
+        frame_output = Frame(window)
+        frame_output.grid()
+        window.title("Результат суммы дробей")
+        result = Text(frame_output)
+        result.grid(column=1, row=1)
+        result.insert(1.0, "{0} + {1} = {2}".format(a_val, b_val, Rationals.ADD_QQ_Q(a_val, b_val)))
+        result.configure(state='disabled')
+
+    try:
+        frame_output.destroy()
+    except:
+        print("none")
+    try:
+        frame_input.destroy()
+    except:
+        print("none")
+    frame.destroy()
+    frame_input = Frame(window)
+    frame_input.grid()
+    window.title("Сумма дробей")
+    a_lab = Label(frame_input, text="Введите первую дробь:").grid(row=1, column=1)
+    a = Entry(frame_input, width=16)
+    a.grid(row=1, column=2, padx=(4, 0))
+    b_lab = Label(frame_input, text="Введите вторую дробь:").grid(row=2, column=1)
+    b = Entry(frame_input, width=16)
+    b.grid(row=2, column=2, padx=(4, 0))
+    but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
+
+# Вычетание дробей
+def SUB_QQ_Q():
+    global frame_output, frame_input
+
+    def RESULT():
+        global frame_output
+        a_val = RNumber(str(a.get()))
+        b_val = RNumber(str(b.get()))
+        frame_input.destroy()
+        frame_output = Frame(window)
+        frame_output.grid()
+        window.title("Результат разности дробей")
+        result = Text(frame_output)
+        result.grid(column=1, row=1)
+        result.insert(1.0, "{0} + {1} = {2}".format(a_val, b_val, Rationals.SUB_QQ_Q(a_val, b_val)))
+        result.configure(state='disabled')
+
+    try:
+        frame_output.destroy()
+    except:
+        print("none")
+    try:
+        frame_input.destroy()
+    except:
+        print("none")
+    frame.destroy()
+    frame_input = Frame(window)
+    frame_input.grid()
+    window.title("Разность дробей")
+    a_lab = Label(frame_input, text="Введите первую дробь:").grid(row=1, column=1)
+    a = Entry(frame_input, width=16)
+    a.grid(row=1, column=2, padx=(4, 0))
+    b_lab = Label(frame_input, text="Введите вторую дробь:").grid(row=2, column=1)
+    b = Entry(frame_input, width=16)
+    b.grid(row=2, column=2, padx=(4, 0))
+    but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
+
+# Умножение дробей
+def MUL_QQ_Q():
+    global frame_output, frame_input
+
+    def RESULT():
+        global frame_output
+        a_val = RNumber(str(a.get()))
+        b_val = RNumber(str(b.get()))
+        frame_input.destroy()
+        frame_output = Frame(window)
+        frame_output.grid()
+        window.title("Результат произведения дробей")
+        result = Text(frame_output)
+        result.grid(column=1, row=1)
+        result.insert(1.0, "{0} + {1} = {2}".format(a_val, b_val, Rationals.MUL_QQ_Q(a_val, b_val)))
+        result.configure(state='disabled')
+
+    try:
+        frame_output.destroy()
+    except:
+        print("none")
+    try:
+        frame_input.destroy()
+    except:
+        print("none")
+    frame.destroy()
+    frame_input = Frame(window)
+    frame_input.grid()
+    window.title("Произведение дробей")
+    a_lab = Label(frame_input, text="Введите первую дробь:").grid(row=1, column=1)
+    a = Entry(frame_input, width=16)
+    a.grid(row=1, column=2, padx=(4, 0))
+    b_lab = Label(frame_input, text="Введите вторую дробь:").grid(row=2, column=1)
+    b = Entry(frame_input, width=16)
+    b.grid(row=2, column=2, padx=(4, 0))
+    but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
+
+# Деление дробей
+def DIV_QQ_Q():
+    global frame_output, frame_input
+
+    def RESULT():
+        global frame_output
+        a_val = RNumber(str(a.get()))
+        b_val = RNumber(str(b.get()))
+        frame_input.destroy()
+        frame_output = Frame(window)
+        frame_output.grid()
+        window.title("Результат частного дробей")
+        result = Text(frame_output)
+        result.grid(column=1, row=1)
+        result.insert(1.0, "{0} + {1} = {2}".format(a_val, b_val, Rationals.DIV_QQ_Q(a_val, b_val)))
+        result.configure(state='disabled')
+
+    try:
+        frame_output.destroy()
+    except:
+        print("none")
+    try:
+        frame_input.destroy()
+    except:
+        print("none")
+    frame.destroy()
+    frame_input = Frame(window)
+    frame_input.grid()
+    window.title("Частное дробей")
+    a_lab = Label(frame_input, text="Введите первую дробь:").grid(row=1, column=1)
+    a = Entry(frame_input, width=16)
+    a.grid(row=1, column=2, padx=(4, 0))
+    b_lab = Label(frame_input, text="Введите вторую дробь:").grid(row=2, column=1)
+    b = Entry(frame_input, width=16)
+    b.grid(row=2, column=2, padx=(4, 0))
+    but = Button(frame_input, text="Рассчитать", width=10, command=RESULT).grid(row=3, column=1, padx=(10, 0))
+
+
 window = Tk()
 window.option_add("*Label.Font", "helvetica 20 bold")
 window.option_add("*Text.Font", "helvetica 20 bold")
 window.title("Коллоквиум ДМ - 2022")
-window.state("zoomed")
-window.geometry("800x200")
+window.geometry("800x500")
 menu = Menu(window)
 natural = Menu(menu, tearoff=0)
 integer = Menu(menu, tearoff=0)
@@ -546,7 +727,7 @@ natural.add_command(label="Умножение чисел", command=MUL_NN_N)
 natural.add_command(label="Неполное частное", command=DIV_NN_N)
 natural.add_command(label="Остаток от деления", command=MOD_NN_N)
 natural.add_command(label="НОД", command=GCF_NN_N)
-natural.add_command(label="НОК")
+natural.add_command(label="НОК", command=LCM_NN_N)
 
 integer.add_command(label="Сумма чисел", command=ADD_ZZ_Z)
 integer.add_command(label="Разность чисел", command=SUB_ZZ_Z)
@@ -554,9 +735,21 @@ integer.add_command(label="Произведение чисел", command=MUL_ZZ_
 integer.add_command(label="Неполное частное", command=DIV_ZZ_Z)
 integer.add_command(label="Остаток от деления", command=MOD_ZZ_Z)
 
-rational.add_command(label="Rational")
+rational.add_command(label="Сокращение дроби", command=RED_Q_Q)
+rational.add_command(label="Сложение дробей", command=ADD_QQ_Q)
+rational.add_command(label="Вычитание дробей", command=SUB_QQ_Q)
+rational.add_command(label="Умножение дробей", command=MUL_QQ_Q)
+rational.add_command(label="Деление дробей", command=DIV_QQ_Q)
 
-polynomials.add_command(label="Polynomials")
+polynomials.add_command(label="Сложение многочленов")
+polynomials.add_command(label="Вычитание многочленов")
+polynomials.add_command(label="Вынесение НОК знаменателей и НОД числителей")
+polynomials.add_command(label="Умножение многочленов")
+polynomials.add_command(label="Неполное частное")
+polynomials.add_command(label="Остаток от деления")
+polynomials.add_command(label="НОД многочленов")
+polynomials.add_command(label="Производная многочлена")
+polynomials.add_command(label="Кратные корни в простые")
 
 menu.add_cascade(label="Натуральные числа", menu=natural)
 menu.add_cascade(label="Целые числа", menu=integer)
