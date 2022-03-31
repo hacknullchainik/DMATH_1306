6eda6d847caebb8cd0f19ec012be482a64f6b9ea
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-03-31 11:14:05 +0300

    Update Integers.py

--- Integers.py
+++ Integers.py
@@ -60,7 +60,7 @@ def MUL_ZZ_Z(num1: Integer, num2: Integer):
 
     # Так как теперь мы имеем 2 натуральных числа можно просто перемножить их
     # соответствующей функцией
-    result = Naturals.MUL_NN_N(lower_num, bigger_num)
+    result = MUL_NN_N(lower_num, bigger_num)
 
     # Знак числа, получаемого при умножении легко вычисляется XOR'ом
     res_sign = sign1 ^ sign2
