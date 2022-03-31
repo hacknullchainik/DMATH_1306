0a8f464083ba25071be48d24418a6b4af886856e
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-03-31 11:15:40 +0300

    Create Polynomials.py

---
+++ Polynomials.py
@@ -0,0 +1,43 @@
+from Dtypes import NNumber, Integer, RNumber, Polynomial
+from Naturals import *
+from Integers import *
+from Rationals import *
+
+# Сложение многочленов
+# ADD_PP_P
+
+# Вычитание многочленов
+# SUB_PP_P
+
+# Умножение многочлена на рациональное число
+# MUL_PQ_P
+
+# Умножение многочлена на х**к
+# MUL_Pxk_P
+
+# Старший коэффициент многочлена
+# LED_P_Q
+
+# Степень многочлена
+# DEG_P_N
+
+# Вынесение НОК/НОД
+# FAC_P_Q
+
+# Умножение многочленов
+# MUL_PP_P
+
+# Целочисленное деление
+# DIV_PP_P
+
+# Остаток от деления
+# MOD_PP_P
+
+# НОД
+# GCF_PP_P
+
+# Производная
+# DER_P_P
+
+# Кратные корни в простые
+# NMR_P_P
