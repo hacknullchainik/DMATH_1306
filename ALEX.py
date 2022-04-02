1feb5a413ec0ca1d5d2ce47fa40047b45cce0cdc
Alex Bulganin <90492542+doal116@users.noreply.github.com>
2022-04-02 14:31:26 +0300

    update

--- ALEX.py
+++ ALEX.py
@@ -1,7 +1,6 @@
 # The present module works with Integer, RNumber, Polynomial, NNumber instances
 # The module providing those classes is 'Dtypes.py'
 
-from typing import final
 from Dtypes import Integer, NNumber, Integer, RNumber, Polynomial
 import Naturals, Integers, Rationals
 
@@ -27,7 +26,11 @@ def MUL_ND_N(num: object, num_2: int):
                 # the first digit of the resulting number of the multilpication is stored inside keeper
                 # the value of keeper is then added to the result of the next multiplication
                 value = value + keeper
-                results.insert(0, value)
+                if value < 10:
+                    results.insert(0, value)
+                else:
+                    results.insert(0,value%10)
+                    results.insert(0,value//10)
                 keeper = 0
         elif keeper != 0:
             # in case the next resulting number of the multiplication also exceeds or is equal to 10
