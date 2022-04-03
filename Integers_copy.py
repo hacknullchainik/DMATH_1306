79757e66e2af43e83bc5fe9768cf9c153cd64109
Sasha Pulinets <sasha.hellsys@gmail.com>
2022-04-03 05:52:33 +0300

    update

--- Integers_copy.py
+++ Integers_copy.py
@@ -1,6 +1,9 @@
 from Integers import *
-import Integers
+import Integers, Naturals
 from Integers_test import Itest
+
+# ВСЕ ПРЕДСТАВЛЕННЫЕ ФУНКЦИИ ПРАВИЛЬНО НЕ РАБОТАЮТ,
+# В ОСНОВНОЙ ФАЙЛ КИДАТЬ НЕ НАДО
 
 def MOD_ZZ_Z(num: Integer, num_2: Integer):
     # 'a' stores the value of the dividend.
@@ -32,26 +35,47 @@ def MOD_ZZ_Z(num: Integer, num_2: Integer):
 
     return r
 
-def SUB_ZZ_Z(num1:Integer, num2:Integer):
+
+def SUB_ZZ_Z(num1: Integer, num2: Integer):
     # - + = - - (сложение)
     # + - = + + (сложение)
     # - - = - + (не сложение)
     # + + = + -  (не сложение)
     if num1.get_sign() != num2.get_sign():
-        res = ADD_NN_N(ABS_Z_N(num1),ABS_Z_N(num2)).get_num()
+        res = ADD_NN_N(ABS_Z_N(num1), ABS_Z_N(num2)).get_num()
         res.reverse()
-        res = Integer(res,num1.get_sign())
+        res = Integer(res, num1.get_sign())
     else:
-        if COM_NN_D(num1,num2)  == 2:
+        if COM_NN_D(num1, num2) == 2:
             sig = num1.get_sign()
         else:
             if num2.get_sign():
                 sig = 0
             else:
                 sig = 1
-        res = SUB_NN_N(ABS_Z_N(num1),ABS_Z_N(num2)).get_num()
+        res = SUB_NN_N(ABS_Z_N(num1), ABS_Z_N(num2)).get_num()
         res.reverse()
-        res = Integer(res,sig)
+        res = Integer(res, sig)
     return res
 
-Itest(['MOD_ZZ_Z', 'SUB_ZZ_Z', ])
+
+def DIV_ZZ_Z(n: Integer, m: Integer):
+    res = []
+    # Проверяем числа на знаки (узнаём, в результате будет положительное число или отрицательное)
+    if (Integers.POZ_Z_D(n) + Integers.POZ_Z_D(m)) == 4:
+        sign = False
+    elif (Integers.POZ_Z_D(n) + Integers.POZ_Z_D(m)) == 3:
+        sign = True
+    else:
+        sign = False
+
+    # Берём абсолютные значения (знак уже запомнили) и применяем обычное деление натуральных чисел
+    # В результате всё равно целое
+    n = Integers.ABS_Z_N(n)
+    m = Integers.ABS_Z_N(m)
+    res = Naturals.DIV_NN_N(n, m).get_num()
+
+    return Integer(res, sign)
+
+
+Itest(['MOD_ZZ_Z', 'SUB_ZZ_Z', 'DIV_ZZ_Z'])
--- Naturals_copy.py
+++ Naturals_copy.py
@@ -5,14 +5,61 @@ from Rationals import *
 from Naturals_test import Ntest
 
 
-def INT_Q_B(num: RNumber):
-    # сокращаем дробь
-    num2 = RED_Q_Q(num)
+def DIV_NN_Dk(num1: NNumber, num2: NNumber):
+    # Этот алгоритм полностью повторяет деление в столбик, если с комментариями будет
+    # что-то непонятно, распишите деление 2-х рандомных чисел и смотря на вашу запись и алгоритм, все поймете
 
-    # првоеряем является ли знаменатель 1 в сокращенной дроби
-    if (num2.get_den() == 1) or (num2.get_num() == 0):
-        return True
-    else:
-        return False
+    # Заносим значения чисел в числовой массив
+    curr_num = 0
+    count = 0
+    template_value = []
+    lower_num = num1.get_num()
+    bigger_num = num2.get_num()
+
+    lower_num.reverse()
+    bigger_num.reverse()
+
+    # Проверяем, действительно ли в переменной, обозначающей большее число
+    # находится большее исло, при необзодимости меняем местами
+
+    if COM_NN_D(num1, num2) == 2:
+        lower_num, bigger_num = bigger_num, lower_num
+
+    # Проверка делителя на 0
+    if not int(NNumber(lower_num).__str__()):
+        raise ZeroDivisionError
+
+    # Берем из большего числа столько цифр, сколько их в меньшем и заносим
+    # в массив с временным значением
+
+    for i in range(len(lower_num)):
+        template_value.append(bigger_num[i])
+        curr_num = i
+
+    # Если так получилось, что число, получившееся на предыдущем шаге меньше
+    # числа, на которое делим, берем ещё одну цифру
+
+    if COM_NN_D(NNumber(lower_num), NNumber(template_value)) == 2:
+        template_value.append(bigger_num[curr_num + 1])
+
+    # k - переменная, обозначающая степень десятки и она разности длин
+    # начального большего числа и получившегося из последних шагов
+
+    k = len(bigger_num) - len(template_value)
+
+    # Пока получившееся число больше меньшего числа, отнимаем от него меньшее
+    # при этом на каждом шаге прибаляем к переменной count 1, эта переменная показывает
+    # на сколько мы "умножили" меьншее число
+
+    while COM_NN_D(NNumber(template_value), NNumber(lower_num)) != 1:
+        sub = SUB_NN_N(NNumber(template_value), NNumber(lower_num))
+        template_value = sub.get_num()
+        template_value.reverse()
+        count += 1
+
+    count = count * pow(10, k)
+
+    return count
+
 
 Ntest(['INT_Q_B'])
