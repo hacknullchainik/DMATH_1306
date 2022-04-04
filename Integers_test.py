127e6816e1d2de3240ca31bf0bfec07dead10985
Sasha Pulinets <sasha.hellsys@gmail.com>
2022-04-04 12:27:25 +0300

    update

--- Integers_test.py
+++ Integers_test.py
@@ -115,15 +115,15 @@ class TestIntegers(unittest.TestCase):
     # python -m unittest Integers_test.TestIntegers.test_MOD_ZZ_Z
     def test_MOD_ZZ_Z(self):
         self.assertEqual(MOD_ZZ_Z(Integer('1'), Integer('1')).__str__(), str(1 % 1))
-        #self.assertEqual(MOD_ZZ_Z(Integer('1'),Integer('-1')).__str__(),str(1%-1))
-        #self.assertEqual(MOD_ZZ_Z(Integer('-1'),Integer('1')).__str__(),str(-1%1))
+        # self.assertEqual(MOD_ZZ_Z(Integer('1'),Integer('-1')).__str__(),str(1%-1))
+        # self.assertEqual(MOD_ZZ_Z(Integer('-1'),Integer('1')).__str__(),str(-1%1))
         self.assertEqual(MOD_ZZ_Z(Integer('-1'), Integer('-1')).__str__(), str(-1 % -1))
         a, b = '-123', '-9'
-        self.assertEqual(MOD_ZZ_Z(Integer(a), Integer(b)).__str__(), str(-123 % -9))
+        self.assertEqual(MOD_ZZ_Z(Integer(a), Integer(b)).__str__(), '3')
         a, b = '123', '-9'
         self.assertEqual(MOD_ZZ_Z(Integer(a), Integer(b)).__str__(), '6')
         a, b = '-123', '9'
-        self.assertEqual(MOD_ZZ_Z(Integer(a), Integer(b)).__str__(), '-6')
+        self.assertEqual(MOD_ZZ_Z(Integer(a), Integer(b)).__str__(), '3')
         a, b = '-123', '0'
         self.assertRaises(ZeroDivisionError, MOD_ZZ_Z, Integer(a), Integer(b))
         a, b = '0', '-9'
